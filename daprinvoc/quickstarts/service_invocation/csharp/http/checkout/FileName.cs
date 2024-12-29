435003f2b717bbface47373006783e64d2762d4e
SanthoshBJ <isroman.san@gmail.com>
2024-12-29 13:07:31 +0530

    correcting the checkin issue

---
+++ daprinvoc/quickstarts/service_invocation/csharp/http/checkout/FileName.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace checkout
+{
+    /// <summary>
+    /// Request dto for Get EventChat
+    /// </summary>
+    public class EventChatQueryDto
+    {
+        [JsonProperty(PropertyName = "EventId")]
+        /// <summary>
+        /// Gets or sets the event identifier.
+        /// </summary>
+        /// <value>
+        /// The event identifier.
+        /// </value>
+        public int EventId { get; set; }
+    }
+
+
+    public class GetUsersByUserIdsRequestDto
+    {
+        public List<int> UserIds { get; set; }
+    }
+
+}
